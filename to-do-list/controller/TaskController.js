f6ab0dc469b9907c8e32d40aa1918eab82f1b732
Luciana Ferreira <50185909+luluthedevil@users.noreply.github.com>
2022-05-20 17:58:29 -0300

    To-do list

---
+++ to-do-list/controller/TaskController.js
@@ -0,0 +1,83 @@
+const Task = require("../models/Task");
+
+let message = "";
+let type = "";
+
+const getAllTasks = async (req, res) => {
+  try {
+    setTimeout(() => {
+      message = "";
+    }, 1000);
+    const tasksList = await Task.find();
+    return res.render("index", {
+      tasksList,
+      task: null,
+      taskDelete: null,
+      message,
+      type,
+    });
+  } catch (err) {
+    res.status(500).send({ error: err.message });
+  }
+};
+
+const createTask = async (req, res) => {
+  const task = req.body;
+
+  if (!task.task) {
+    message = "Type something before adding a task!";
+    type = "danger";
+    return res.redirect("/");
+  }
+
+  try {
+    await Task.create(task);
+    message = "Task succefuly created!";
+    type = "success";
+    return res.redirect("/");
+  } catch (err) {
+    res.status(500).send({ error: err.message });
+  }
+};
+
+const getById = async (req, res) => {
+  try {
+    const tasksList = await Task.find();
+    if (req.params.method == "delete") {
+      const taskDelete = await Task.findOne({ _id: req.params.id });
+      res.render("index", { task: null, taskDelete, tasksList, message, type });
+    }
+  } catch (err) {
+    res.status(500).send({ error: err.message });
+  }
+};
+
+const deleteOneTask = async (req, res) => {
+  try {
+    await Task.deleteOne({ _id: req.params.id });
+    message = "Task deleted succefuly!";
+    type = "success";
+    res.redirect("/");
+  } catch (err) {
+    res.status(500).send({ error: err.message });
+  }
+};
+
+const taskCheck = async (req, res) => {
+  try {
+    const task = await Task.findOne({ _id: req.params.id });
+    task.check ? task.check = false : task.check = true;
+    await Task.updateOne({ _id: req.params.id }, task);
+    res.redirect("/");
+  } catch (err) {
+    res.status(500).send({ error: err.message });
+  }
+};
+
+module.exports = {
+  getAllTasks,
+  createTask,
+  getById,
+  deleteOneTask,
+  taskCheck,
+};
